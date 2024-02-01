32f19b77e9c09e3d127ff345758a840aa361ffd3
ViniciusDevAssis <viniciusfsa2014@gmail.com>
2024-02-01 15:23:38 -0300

    Primeiro commit

---
+++ src/main/java/com/viniciusdevassis/cursospringreact/CursospringreactApplication.java
@@ -0,0 +1,13 @@
+package com.viniciusdevassis.cursospringreact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursospringreactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursospringreactApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/viniciusdevassis/cursospringreact/CursospringreactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.viniciusdevassis.cursospringreact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursospringreactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
